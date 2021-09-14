592f4216633bda0b8184c6995d26684ed6e00148
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-09-14 02:24:02 +0200

    new files

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+const io = socketio(server);
+
+const botName = 'KittyChat bot';
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser} = require('./utils/users')
+
+const clientPath = `${__dirname}/./client`;
+app.use(express.static(clientPath));
+
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+
+    socket.on('joinRoom', ({username, chatroom}) => {
+        const user = userJoin(socket.id, username, chatroom);
+
+        socket.join(user.chatroom);
+        console.log(user.chatroom)
+
+        // Emit sends message to everybody when client connects
+        socket.emit('message', formatMessage(botName, `😻 Hi ${user.username}, welcome to KittyChat - ${user.chatroom}! 😻`));
+
+        // Send message when user connects, to everybody except client
+        socket.broadcast.to(user.chatroom).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        // Runs when client disconnects
+        socket.on('disconnect', () => {
+            io.emit('message', formatMessage(botName, `${user.username} has left the chat`));
+        });
+    });
+
+    // Send message to everybody
+    socket.on('sendToAll', (message) => {
+        const user = getCurrentUser(socket.id);
+
+        io.emit("message", formatMessage(user.username, message));
+    });
+
+    // Send message only to me
+    socket.on('sendToMe', (message) => {
+        socket.emit("message", formatMessage(user.username, message));
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`server running on ${port}`)
+});
